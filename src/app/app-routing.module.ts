69f0715410509a99ae931c45a559952faecffbe8
Juan Carlos <juan.diaz@sstrategy.io>
2023-07-18 18:07:59 +0200

    Fin seccion 4 -router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { PagesRoutingModule } from './pages/pages-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+
 
 const routes:Routes=[
 
-  { path:'',
-    component:PagesComponent,
-    children:[
-            { path:'dashboard',component:DashboardComponent},
-            { path:'grafica1',component:Grafica1Component},
-            { path:'progress',component:ProgressComponent},  
-            { path:'', redirectTo:'/dashboard' ,pathMatch:'full'},       
-    ]
-  },
-
-  { path:'login',component:LoginComponent},
-  { path:'register',component:RegisterComponent},
-  
-
+  // { path:'',
+  //   loadChildren: ()=> import ( './pages/pages.module').then( m => m.PagesModule )
+  // },
+  // { path:'register',component:RegisterComponent},
+  // { path:'login',component:LoginComponent},
+  { path:'', redirectTo:'/dashboard',pathMatch:'full' },
   { path:'**',component:NopagefoundComponent },
+
 
 ]
 
 @NgModule({
-  declarations: [],
-  imports: [ RouterModule.forRoot( routes )],
+  imports: [ 
+            RouterModule.forRoot( routes ),
+            AuthModule,
+            PagesModule
+          ],
   exports:[ RouterModule ]
 })
 export class AppRoutingModule { }
